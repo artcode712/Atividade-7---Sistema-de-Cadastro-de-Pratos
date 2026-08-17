238ffdd63c0eb42497ee555365538bf213fceddc
arthur_heimann <arthur_heimann@estudante.sesisenai.org.br>
2026-08-17 09:23:34 -0300

    add- adicionei tanto no db quanto no conexao, mas apaguei as duas livrarias pq a gente precisa criar uma em que os dois possam acessar.

---
+++ database/db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE livraria_;
+USE livraria_;
+
+CREATE TABLE livros (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    autor VARCHAR(100)NOT NULL,
+    ano INT NOT NULL
+);

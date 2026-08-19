66ea6f10fcf0674268b26e2f9a825a6be60750f4
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-08-18 21:32:40 -0300

    add - página simples de index foi introduzida conectando à página de cadastro, onde tanto clientes quanto pratos poderão ser cadastrados, e de listar, onde poderá ser editado ou excluído qualquer cadastro realizado.

---
+++ style/index.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurante</title>
+
+    <link rel="stylesheet" href="style/index.php">
+
+</head>
+
+<body>
+
+    <h1>Sistema do Restaurante</h1>
+
+    <a href="public/cadastro.php">Cadastrar</a>
+    <a href="public/listar.php">Listar pratos</a>
+    
+</body>
+</html>
